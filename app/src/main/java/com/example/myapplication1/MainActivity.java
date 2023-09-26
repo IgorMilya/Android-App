e9bca7faf7151ffceac91275ef48fe3711fda602
imilay11 <imilay11@gmail.com>
2023-09-26 15:36:33 +0300

    Added button and function that change text in TextView

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textViewOppenheimer = findViewById(R.id.textOppenheimer);
+        Button buttonChangeText = findViewById(R.id.changeTextButton);
+
+        buttonChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Change the text of the TextView when the button is clicked
+                textViewOppenheimer.setText("New Text is generated");
+            }
+        });
+
     }
 }
